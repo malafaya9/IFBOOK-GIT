99ebca662bf697b2535427829897ba33e9c8e2a6
vinic <vinic@M9>
2016-09-30 10:27:53 -0300

    Add project files.

---
+++ src/IFBOOK/Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace IFBOOK.Models
+{
+    //Aqui estão armazenadas as propriedades personalizadas do ApplicationUser(IdentityUser)
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Nome { get; set; }
+        [Required]
+        [StringLength(14, MinimumLength = 14, ErrorMessage = "A Matrícula deverá possuir 14 digitos.")]
+        public string Matricula { get; set; }
+
+    }
+}
