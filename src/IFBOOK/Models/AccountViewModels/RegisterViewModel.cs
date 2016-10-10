b4611aa97e34192cd70383b9d661f40d16bcacdd
Vinicius Malafaya <malafaya9@gmail.com>
2016-10-10 11:35:23 -0300

    Adicionada a classe modelo para a tabela de sugestões

--- src/IFBOOK/Models/AccountViewModels/RegisterViewModel.cs
+++ src/IFBOOK/Models/AccountViewModels/RegisterViewModel.cs
@@ -16,12 +16,12 @@ namespace IFBOOK.Models.AccountViewModels
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Senha")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Confirmar Senha")]
+        [Compare("Senha", ErrorMessage = "Os campos não possuem o mesmo valor.")]
         public string ConfirmPassword { get; set; }
 
         [Required]
---
+++ src/IFBOOK/Models/Sugestao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IFBOOK.Models
+{
+    public class Sugestao
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Nome { get; set; }
+    }
+}
