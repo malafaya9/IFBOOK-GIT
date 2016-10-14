d10d3cef8c7f3c748bbb33a23d41767089a25573
Vinicius Malafaya <malafaya9@gmail.com>
2016-10-14 10:22:09 -0300

    Adicionada a classe modelo para a tabela TipoAvaliacao

---
+++ src/IFBOOK/Models/TipoAvaliacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IFBOOK.Models
+{
+    public class TipoAvaliacao
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Descricao { get; set; }
+    }
+}
