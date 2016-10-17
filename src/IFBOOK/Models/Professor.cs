552f3463367c5344fbd6453a46a2a9ed4e918c81
Vinicius Malafaya <malafaya9@gmail.com>
2016-10-17 09:55:25 -0300

    Adicionada a classe modelo para a tabela Professores

---
+++ src/IFBOOK/Models/Professor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IFBOOK.Models
+{
+    public class Professor
+    {
+        public int ID { get; set; }
+        [Required]
+        [MaxLength(500)]
+        public int Nome { get; set; }
+        [Required]
+        public float Nota { get; set; }
+    }
+}
