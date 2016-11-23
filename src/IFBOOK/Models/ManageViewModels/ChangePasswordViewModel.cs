b0e0682ce3251270f6c08f52f624e1993392c33f
marcosadriano99 <marcosadriano99@hotmail.com>
2016-11-23 12:44:29 -0200

    Alteração interface

--- src/IFBOOK/Models/ManageViewModels/ChangePasswordViewModel.cs
+++ src/IFBOOK/Models/ManageViewModels/ChangePasswordViewModel.cs
@@ -10,18 +10,18 @@ namespace IFBOOK.Models.ManageViewModels
     {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "Senha atual")]
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "A {0} deve ter pelo menos {2} e no máximo {1} caracteres.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Nova senha")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Display(Name = "Confirmar senha")]
+        [Compare("NewPassword", ErrorMessage = "As senhas não coincidem.")]
         public string ConfirmPassword { get; set; }
     }
 }
