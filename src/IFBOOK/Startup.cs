b500939de1636e76bd9d0bb52314f4c4539e7e97
marcosadriano99 <marcosadriano99@hotmail.com>
2016-12-12 11:28:27 -0200

    Fim...Eu espero

--- src/IFBOOK/Startup.cs
+++ src/IFBOOK/Startup.cs
@@ -41,7 +41,7 @@ namespace IFBOOK
         {
             // Add framework services.
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("Valera")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
